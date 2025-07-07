9db11a4b368d3fa176710c0865379d01d376e2dc
alberto <albertopuet8@gmail.com>
2025-07-07 17:39:10 -0300

    cammit

--- src/app/components/Testimonios.tsx
+++ src/app/components/Testimonios.tsx
@@ -1,24 +1,71 @@
-'use client';
+"use client";
 
-import React, { useRef,  } from 'react';
-import { motion } from 'framer-motion';
-import { FaQuoteLeft } from 'react-icons/fa';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+import { FaQuoteLeft } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const testimonios = [
-  { nombre: 'María Fernández', texto: 'Antes me paralizaba al hablar en publico, gracias a Martin me desenvuelvo mejor a la hora de hablar.', profesion: 'Emprendedora' },
-  { nombre: 'Lucas Martínez', texto: 'Con el entrenamiento de Martin pude presentarme a entrevistas con mayor confianza.', profesion: 'Diseñador UX' },
-  { nombre: 'Sofía Ramírez', texto: 'Era escéptica, pero después de la primera sesión sentí un cambio notable en mi confianza al hablar en publico.', profesion: 'Abogada' },
-  { nombre: 'Carlos Ruiz', texto: 'Martin me ayudo a desarrollar una voz más segura.', profesion: 'Estudiante de Derecho' },
-  { nombre: 'Ana Torres', texto: 'Noté mejoras desde la segunda clase, la forma de explicar de Martin es muy clara', profesion: 'Psicóloga' },
-  { nombre: 'Tomás Delgado', texto: 'El cambio ha sido increíble. Ya no tiemblo al presentar frente a mucha gente.', profesion: 'Arquitecto' },
-  { nombre: 'Lucía Méndez', texto: 'Pude dar mi primera charla en una conferencia gracias a este entrenamiento.', profesion: 'Coach de vida' },
-  { nombre: 'Julián Pérez', texto: 'Me ayudaron a soltar la voz y encontrar un estilo propio.', profesion: 'Actor' },
-  { nombre: 'Verónica Díaz', texto: 'Gracias a este entrenamiento pude enfrentar mis miedos al hablar en publico.', profesion: 'Gerente de marketing' },
-  { nombre: 'Martín López', texto: 'Este entrenamiento fue clave para mi crecimiento profesional.', profesion: 'Contador público' },
+  {
+    nombre: "María Fernández",
+    texto:
+      "Antes me paralizaba al hablar en publico, gracias a Martin me desenvuelvo mejor a la hora de hablar.",
+    profesion: "Emprendedora",
+  },
+  {
+    nombre: "Lucas Martínez",
+    texto:
+      "Con el entrenamiento de Martin pude presentarme a entrevistas con mayor confianza.",
+    profesion: "Diseñador UX",
+  },
+  {
+    nombre: "Sofía Ramírez",
+    texto:
+      "Era escéptica, pero después de la primera sesión sentí un cambio notable en mi confianza al hablar en publico.",
+    profesion: "Abogada",
+  },
+  {
+    nombre: "Carlos Ruiz",
+    texto: "Martin me ayudo a desarrollar una voz más segura.",
+    profesion: "Estudiante de Derecho",
+  },
+  {
+    nombre: "Ana Torres",
+    texto:
+      "Noté mejoras desde la segunda clase, la forma de explicar de Martin es muy clara",
+    profesion: "Psicóloga",
+  },
+  {
+    nombre: "Tomás Delgado",
+    texto:
+      "El cambio ha sido increíble. Ya no tiemblo al presentar frente a mucha gente.",
+    profesion: "Arquitecto",
+  },
+  {
+    nombre: "Lucía Méndez",
+    texto:
+      "Pude dar mi primera charla en una conferencia gracias a este entrenamiento.",
+    profesion: "Coach de vida",
+  },
+  {
+    nombre: "Julián Pérez",
+    texto: "Me ayudaron a soltar la voz y encontrar un estilo propio.",
+    profesion: "Actor",
+  },
+  {
+    nombre: "Verónica Díaz",
+    texto:
+      "Gracias a este entrenamiento pude enfrentar mis miedos al hablar en publico.",
+    profesion: "Gerente de marketing",
+  },
+  {
+    nombre: "Martín López",
+    texto: "Este entrenamiento fue clave para mi crecimiento profesional.",
+    profesion: "Contador público",
+  },
 ];
 
 export default function Testimonios() {
@@ -37,8 +84,6 @@ export default function Testimonios() {
           Lo que dicen quienes ya entrenaron su voz
         </motion.h2>
 
-        
-
         <Swiper
           modules={[Autoplay, Navigation]}
           spaceBetween={20}
@@ -55,7 +100,10 @@ export default function Testimonios() {
           }}
           onInit={(swiper) => {
             // Esto asegura que Swiper reconozca los botones personalizados después de montar el DOM
-            if (typeof swiper.params.navigation !== 'boolean') {
+            if (
+              swiper.params.navigation &&
+              typeof swiper.params.navigation !== "boolean"
+            ) {
               swiper.params.navigation.prevEl = prevRef.current;
               swiper.params.navigation.nextEl = nextRef.current;
               swiper.navigation.init();
@@ -65,21 +113,26 @@ export default function Testimonios() {
         >
           {testimonios.map((testimonio, index) => (
             <SwiperSlide key={index}>
-            <motion.div
-              initial={{ opacity: 0, y: 40 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3 }}
-              className="bg-white hover:cursor-pointer shadow-xl rounded-2xl p-6 text-left border-l-4 border-[#cf7806] h-full flex flex-col justify-between min-h-[280px]"
-            >
-              <FaQuoteLeft className="text-[#cf7806] text-3xl mb-4" />
-              <p className="text-gray-700 text-sm sm:text-base mb-4">{testimonio.texto}</p>
-              <div>
-                <p className="font-semibold text-[#0A2647]">{testimonio.nombre}</p>
-                <p className="text-[#cf7806] text-sm">{testimonio.profesion}</p>
-              </div>
-            </motion.div>
-          </SwiperSlide>
-          
+              <motion.div
+                initial={{ opacity: 0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+                className="bg-white hover:cursor-pointer shadow-xl rounded-2xl p-6 text-left border-l-4 border-[#cf7806] h-full flex flex-col justify-between min-h-[280px]"
+              >
+                <FaQuoteLeft className="text-[#cf7806] text-3xl mb-4" />
+                <p className="text-gray-700 text-sm sm:text-base mb-4">
+                  {testimonio.texto}
+                </p>
+                <div>
+                  <p className="font-semibold text-[#0A2647]">
+                    {testimonio.nombre}
+                  </p>
+                  <p className="text-[#cf7806] text-sm">
+                    {testimonio.profesion}
+                  </p>
+                </div>
+              </motion.div>
+            </SwiperSlide>
           ))}
         </Swiper>
       </div>
